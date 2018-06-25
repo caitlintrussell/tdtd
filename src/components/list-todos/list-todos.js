7b78c8f6d1d3fb1b21a645443e14d162b83bcd33
Caitlin Trussell <caitlintrussell@gmail.com>
2018-06-25 17:32:37 -0500

    fixed toggle wooooo

--- src/components/list-todos/list-todos.js
+++ src/components/list-todos/list-todos.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import SingleTodo from '../single-todo/single-todo';
 
-const ListTodos = () => (
-  <SingleTodo />
-);
+const initialState = [
+  {
+    id: 1,
+    title: 'Go Grocery Shopping',
+    description: 'Produce at Stanleys, pantry goods at Fresh Thyme',
+    passing: true,
+  },
+  {
+    id: 2,
+    title: 'Learn React Debugging',
+    description: 'Download React Devtools, watch Tech Talk',
+    passing: false,
+  },
+  {
+    id: 3,
+    title: 'Review Pillars',
+    description: 'Read notes from my awesome learning team leader',
+    passing: false,
+  },
+  {
+    id: 4,
+    title: 'Practice React Routing',
+    description: 'Watch workshop solution video and ',
+    passing: false,
+  },
+  {
+    id: 5,
+    title: 'Go to the React Developers Meetup',
+    description: 'On Wednesday, mix and mingle with other React lovers like me',
+    passing: false,
+  },
+];
+
+class ListTodos extends Component {
+  state = {todos: initialState};
+
+  toggleTodo = (e) => {
+    const changedTodos = this.state.todos;
+    changedTodos[+e.target.name - 1].passing = !changedTodos[+e.target.name - 1].passing;
+    this.setState({todos: changedTodos});
+  }
+  render() {
+    const {todos} = this.state;
+    return (
+      todos.map((todo) => (
+        <SingleTodo key={todo.id} todo={todo} toggleTodo={this.toggleTodo} />
+      ))
+    );
+  }
+
+}
+
+
 ListTodos.displayName = 'ListTodos';
 export default ListTodos;
+
--- src/components/single-todo/single-todo.js
+++ src/components/single-todo/single-todo.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './single-todo.css';
 
-const todo = {
-  name: 'Learn React Debugging',
-  description: 'Download React Devtools, watch Tech Talk',
-  passing: false,
-};
-// MIGRATE TO ITERABLE W PROPS.TODO, not dummy data !
-const SingleTodo = () => (
+
+const SingleTodo = (props) => {
+  const {todo} = props;
+  return (
   <div className="singleTodo-container">
     <div className="singleTodo-toggle">
-      <input name="passing" className="checkbox" type="checkbox" id="toggle" checked={todo.passing} />
-      <label htmlFor="toggle" />
+      <input name={todo.id} className="checkbox" type="checkbox" id={`toggle${todo.id}`} checked={todo.passing} onChange={props.toggleTodo} />
+      <label htmlFor={`toggle${todo.id}`} />
     </div>
     <div className="singleTodo-todo">
-      <h4>{todo.name}</h4>
+      <h4>{todo.title}</h4>
       <p>{todo.description}</p>
     </div>
   </div>
-);
+  );
+};
 
 SingleTodo.displayName = 'SingleTodo';
 export default SingleTodo;
