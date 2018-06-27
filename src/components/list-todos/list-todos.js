ac3fd58aa2a0292af47cb968a8a116be13297077
Caitlin Trussell <caitlintrussell@gmail.com>
2018-06-26 22:46:07 -0500

    branch for key bug

--- src/components/list-todos/list-todos.js
+++ src/components/list-todos/list-todos.js
@@ -3,18 +3,12 @@ import SingleTodo from '../single-todo/single-todo';
 
 
 class ListTodos extends Component {
-  // state = {todos: initialState};
   displayName = 'ListTodos';
-  // toggleTodo = (e) => {
-  //   const changedTodos = this.state.todos;
-  //   changedTodos[+e.target.name - 1].passing = !changedTodos[+e.target.name - 1].passing;
-  //   this.setState({todos: changedTodos});
-  // }
   render() {
     const {todos, toggleTodo} = this.props;
     return (
       todos.map((todo) => (
-        <SingleTodo key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+        <SingleTodo todo={todo} toggleTodo={toggleTodo} />
       ))
     );
   }
