c18a319a816c7c659829e0efba61aef9c3fbe628
Caitlin Trussell <caitlintrussell@gmail.com>
2018-06-26 22:19:51 -0500

    extracted data

--- src/components/main/main-wrapper.js
+++ src/components/main/main-wrapper.js
@@ -3,47 +3,10 @@ import React, {Component} from 'react';
 import ListTodos from '../list-todos/list-todos';
 import AddTodo from '../add-todo/add-todo';
 import './main-wrapper.css';
+import todos from '../sample-todo';
 
-const initialState = [
-  {
-    id: 1,
-    name: 'Go Grocery Shopping',
-    description: 'Produce at Stanleys, pantry goods at Fresh Thyme',
-    passing: true,
-  },
-  {
-    id: 2,
-    name: 'Learn React Debugging',
-    description: 'Download React Devtools, watch Tech Talk',
-    passing: false,
-  },
-  {
-    id: 3,
-    name: 'Review Pillars',
-    description: 'Read notes from my awesome learning team leader',
-    passing: false,
-  },
-  {
-    id: 4,
-    name: 'Practice React Routing',
-    description: 'Watch workshop solution video and read documentation',
-    passing: false,
-  },
-  {
-    id: 5,
-    name: 'Go to the React Developers Meetup',
-    description: 'On Wednesday, mix and mingle with other React lovers like me',
-    passing: false,
-  },
-  {
-    id: 6,
-    name: 'Call Mom',
-    description: 'Because you probably should',
-    passing: false,
-  }
-];
 class Main extends Component {
-  state = {todos: initialState}
+  state = {todos}
   displayName = 'Main'
   toggleTodo = (e) => {
     const changedTodos = this.state.todos.map((todo) => {
